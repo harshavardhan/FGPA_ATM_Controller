53dccf7a6d6f33e49aff53ee889edf7dfd9edf4a
Yashasvi Sriram <yash.3997@gmail.com>
2017-03-20 00:25:00 +0530

    adds : SampleBackEndDatabase.csv data sample edits : top_level.vhdl 	1. adds four new ports 		next_data_in_button  	: in  STD_LOGIC; 		reset_button 		 	: in  STD_LOGIC; 		start_button 			: in  STD_LOGIC; 		done_button 			: in  STD_LOGIC 	2. declares and creates instances debouncer, encrypter, decrypter
    	3. connects the above instances with signals
    
    	4. adds a signal
    	   	signal system_state : STD_LOGIC_VECTOR(2 downto 0) := (others => '0');
    	   which maintains the state of the sytem ex: Ready, Taking Input etc..
    
    	5. creates a main_process : process (clk)
    	   which handles the change of system states and what occurs in each one of them
    
    	6. Implements Ready state and Taking Input state

--- makestuff/hdlmake/apps/makestuff/swled/templates/fx2all/vhdl/top_level.vhdl
+++ makestuff/hdlmake/apps/makestuff/swled/templates/fx2all/vhdl/top_level.vhdl
@@ -1,19 +1,3 @@
---
--- Copyright (C) 2009-2012 Chris McClelland
---
--- This program is free software: you can redistribute it and/or modify
--- it under the terms of the GNU Lesser General Public License as published by
--- the Free Software Foundation, either version 3 of the License, or
--- (at your option) any later version.
---
--- This program is distributed in the hope that it will be useful,
--- but WITHOUT ANY WARRANTY; without even the implied warranty of
--- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
--- GNU Lesser General Public License for more details.
---
--- You should have received a copy of the GNU Lesser General Public License
--- along with this program.  If not, see <http://www.gnu.org/licenses/>.
---
 library ieee;
 
 use ieee.std_logic_1164.all;
@@ -41,10 +25,41 @@ entity top_level is
 		anode_out      : out   std_logic_vector(3 downto 0); -- seven-segment display anodes (one for each digit)
 		led_out        : out   std_logic_vector(7 downto 0); -- eight LEDs
 		sw_in          : in    std_logic_vector(7 downto 0)  -- eight switches
+
+		-- ATM: ----------------------------------------------------------------------------------------
+		next_data_in_button  	: in  STD_LOGIC;
+		reset_button 		 	: in  STD_LOGIC;
+		start_button 			: in  STD_LOGIC;
+		done_button 			: in  STD_LOGIC
 	);
 end entity;
 
 architecture structural of top_level is
+	-- Entities recquired
+	component debouncer
+		port(clk        : in  STD_LOGIC;
+			 button     : in  STD_LOGIC;
+			 button_deb : out STD_LOGIC);
+	end component;
+
+	component encrypter
+		port(clk        : in  STD_LOGIC;
+			 reset      : in  STD_LOGIC;
+			 plaintext  : in  STD_LOGIC_VECTOR(63 downto 0);
+			 start      : in  STD_LOGIC;
+			 ciphertext : out STD_LOGIC_VECTOR(63 downto 0);
+			 done       : out STD_LOGIC);
+	end component;
+
+	component decrypter
+		port(clk        : in  STD_LOGIC;
+			 reset      : in  STD_LOGIC;
+			 ciphertext : in  STD_LOGIC_VECTOR(63 downto 0);
+			 start      : in  STD_LOGIC;
+			 plaintext  : out STD_LOGIC_VECTOR(63 downto 0);
+			 done       : out STD_LOGIC);
+	end component;
+
 	-- Channel read/write interface -----------------------------------------------------------------
 	signal chanAddr  : std_logic_vector(6 downto 0);  -- the selected channel (0-127)
 
@@ -64,6 +79,27 @@ architecture structural of top_level is
 
 	-- Reset signal so host can delay startup
 	signal fx2Reset  : std_logic;
+
+	-- Modification
+	signal debounced_next_data  : STD_LOGIC                     := '0';
+	--signal debounced_next_data_out_button : STD_LOGIC                     := '0';
+	signal start_encrypt : STD_LOGIC                     := '0';
+	signal start_decrypt : STD_LOGIC                     := '0';
+	signal multi_byte_data_read           : STD_LOGIC_VECTOR(63 downto 0) := (others => '0');
+	signal ciphertext_out                 : STD_LOGIC_VECTOR(63 downto 0) := (others => '0');
+	signal plaintext_out                  : STD_LOGIC_VECTOR(63 downto 0) := (others => '0');
+	signal data_to_be_displayed           : STD_LOGIC_VECTOR(63 downto 0) := (others => '0');
+	signal encrypted_response			  : STD_LOGIC_VECTOR(63 downto 0) := (others => '0');
+	signal encryption_over                : STD_LOGIC                     := '1';
+	signal decryption_over                : STD_LOGIC                     := '1';
+	signal system_state                   : STD_LOGIC_VECTOR(2 downto 0) := (others => '0');
+	signal debounced_reset                : STD_LOGIC                     := '0';
+	signal debounced_start                : STD_LOGIC                     := '0';
+	signal debounced_done                 : STD_LOGIC                     := '0';
+	signal n2000 	: STD_LOGIC_VECTOR(7 downto 0) := (others => '0');	
+	signal n1000 	: STD_LOGIC_VECTOR(7 downto 0) := (others => '0');	
+	signal n500 	: STD_LOGIC_VECTOR(7 downto 0) := (others => '0');	
+	signal n100 	: STD_LOGIC_VECTOR(7 downto 0) := (others => '0');	
 begin
 	-- CommFPGA module
 	fx2Read_out <= fx2Read;
@@ -71,6 +107,7 @@ begin
 	fx2Addr_out(0) <=  -- So fx2Addr_out(1)='0' selects EP2OUT, fx2Addr_out(1)='1' selects EP6IN
 		'0' when fx2Reset = '0'
 		else 'Z';
+
 	comm_fpga_fx2 : entity work.comm_fpga_fx2
 		port map(
 			clk_in         => fx2Clk_in,
@@ -96,25 +133,134 @@ begin
 			f2hReady_out   => f2hReady
 		);
 
-	-- Switches & LEDs application
-	swled_app : entity work.swled
-		port map(
-			clk_in       => fx2Clk_in,
-			reset_in     => '0',
+ 	-- ATM requirements
+	data_in_debouncer : debouncer
+		port map(clk        => clk,
+			     button     => next_data_in_button,
+			     button_deb => debounced_next_data);
+
+	start_debouncer : debouncer
+		port map(clk        => clk,
+			     button     => start_button,
+			     button_deb => debounced_start);
+
+	done_debouncer: debouncer
+		port map(clk        => clk,
+			     button     => done_button,
+			     button_deb => debounced_done);
+
+	reset_debouncer : debouncer
+		port map(clk        => clk,
+			     button     => reset,
+			     button_deb => debounced_reset);
+
+	encrypt : encrypter
+		port map(clk        => clk,
+			     reset      => debounced_reset,
+			     plaintext  => multi_byte_data_read,
+			     start      => start_encrypt,
+			     ciphertext => ciphertext_out,
+			     done       => encryption_over);
+
+	decrypt : decrypter
+		port map(clk        => clk,
+			     reset      => debounced_reset,
+			     ciphertext => encrypted_response,
+			     start      => start_decrypt,
+			     plaintext  => plaintext_out,
+			     done       => decryption_over);
 			
-			-- DVR interface -> Connects to comm_fpga module
-			chanAddr_in  => chanAddr,
-			h2fData_in   => h2fData,
-			h2fValid_in  => h2fValid,
-			h2fReady_out => h2fReady,
-			f2hData_out  => f2hData,
-			f2hValid_out => f2hValid,
-			f2hReady_in  => f2hReady,
-			
-			-- External interface
-			sseg_out     => sseg_out,
-			anode_out    => anode_out,
-			led_out      => led_out,
-			sw_in        => sw_in
-		);
+
+--	steer_enc_or_dec_output_to_display : process(clk, start_encrypt, start_decrypt)
+----		variable myline : line;
+--	begin
+--		if (clk'event AND clk = '1') then
+--			if ((start_encrypt = '1') AND (start_decrypt = '0')) then
+--				system_state <= '0';
+--			elsif ((start_encrypt = '0') AND (start_decrypt = '1')) then
+--				system_state <= '1';
+--			end if;
+--		end if;
+
+--	--		write(myline, string'("system state = "));
+--	--		write(myline, system_state);
+--	--		writeline(output, myline);
+--	end process;
+
+	main_process : process(clk)
+		--Input 8 bytes one by one 
+		variable input_byte_count  	 	: integer range 0 to 8       := 0;
+		variable input_taken_already 	: std_logic := '0';
+		variable input_eight_bytes   	: std_logic_vector(63 downto 0) := (others => '0');
+
+	begin
+		if debounced_reset = '1' then
+			-- Resetting Input variables
+			input_byte_count  := 0;
+			input_eight_bytes <= (others => '0');
+			input_taken_already := '0';
+
+		elsif (clk'event AND clk = '1') then
+		
+			-- States described below
+			-- Ready
+			if(system_state = "000") then 
+				-- Start button pressed, ready -> taking input
+				if (debounced_start = '1') then
+					system_state <= "001";
+				end if;
+
+			-- Taking Input
+			elsif (system_state = "001") then
+				--If debounced_next_data is pressed read the 8 bit slide input
+				--If it is pressed more than 8 times, nothing happens
+				
+				-- debounced_next_data == 1 doesnot mean updating registers 
+				-- The former value will be 1 for atleast the clk_time_period * no_cycles_in_debouncer
+				if debounced_next_data = '1' and input_byte_count < 8 then
+					-- If this byte not already taken 
+					if input_taken_already = '0' then
+						-- Update the registers
+						input_eight_bytes(8 * (input_byte_count + 1) - 1 downto 8 * input_byte_count) := sw_in;
+						-- Now this byte is taken
+						input_taken_already := '1';
+						-- Update the byte_count
+						input_byte_count := input_byte_count + 1;
+						--When pressed for 8th time change the system state to "Encrypt" mode
+						if input_byte_count = 8 then
+							system_state <= "010"
+						end if;
+					end if;
+
+				-- debounced_next_data == 0 can only exist right after start 
+				-- or after releasing next_data 
+				elsif debounced_next_data = '0' then
+					-- In all cases input_taken_already can be assigned to zero 
+					input_taken_already := '0';
+				end if;
+
+			-- Encrypt
+			elsif (system_state = "010") then 
+
+			-- Communicating wih backend
+			elsif (system_state = "011") then 
+
+			-- Decrypt
+			elsif (system_state = "100") then 
+
+			-- Loading cash
+			elsif (system_state = "101") then 
+
+			-- Dispensing cash
+			elsif (system_state = "110") then 
+
+			-- Invalid user
+			elsif (system_state = "111") then 
+
+			end if;
+		end if;
+	end process;
+
+	--data_to_be_displayed <= ciphertext_out when (system_state = '0') else plaintext_out; -- edit
+	--done                 <= encryption_over AND decryption_over;
 end architecture;
