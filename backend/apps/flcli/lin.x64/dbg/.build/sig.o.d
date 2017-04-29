f445d689adf730a259b1d05ca99c5fd36e0c7761
Anirudh <anirudhreddyk98@gmail.com>
2017-04-29 18:55:19 +0530

    adds: BankID checking funtionality in the front end. Contact server only when the bank id matches with the last 5 bits of the userid

--- backend/apps/flcli/lin.x64/dbg/.build/sig.o.d
+++ backend/apps/flcli/lin.x64/dbg/.build/sig.o.d
@@ -1,4 +1,4 @@
 lin.x64/dbg/.build/sig.o: sig.c \
- /home/pandu/Desktop/DigitalLogicDesign/Lab/04-connectingatms/backend/common/makestuff.h
+ /home/anirudh/Downloads/DLD-LAB/04-connectingatms/backend/common/makestuff.h
 
-/home/pandu/Desktop/DigitalLogicDesign/Lab/04-connectingatms/backend/common/makestuff.h:
+/home/anirudh/Downloads/DLD-LAB/04-connectingatms/backend/common/makestuff.h:
